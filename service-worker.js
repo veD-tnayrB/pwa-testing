72248bfd2652ae27ca6d7635884d2d52bb1e4f78
veD-tnayrB <Brayanmc.contacto@gmail.com>
2024-11-15 13:37:42 -0400

    fix: solve the cache saving

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const ASSETS_CACHE = 'test';
-const assets = ['/', '/page-2.html', '/src/index.js', '/src/page-2.js', '/public/logo.png'];
+const assets = ['/', '/index.html', '/public/page-2.html', '/src/index.js', '/src/page-2.js', '/public/logo.png'];
 
 self.addEventListener('install', event => {
 	event.waitUntil(
@@ -18,14 +18,12 @@ self.addEventListener('fetch', event => {
 				// Si la solicitud es exitosa, clona la respuesta y la almacena en caché
 				const responseClone = response.clone();
 				caches.open(ASSETS_CACHE).then(cache => {
-					console.log('REQUEST: ', event.request);
 					cache.put(event.request, responseClone);
 				});
 				return response;
 			})
 			.catch(error => {
 				// Si la solicitud falla (por ejemplo, sin conexión), devuelve el recurso de la caché
-				console.log('EMERGENCY CACHE: ', error);
 				return caches.match(event.request);
 			})
 	);
